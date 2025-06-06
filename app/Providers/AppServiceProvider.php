d51066e53a979644e4a9a1244eb6e28a5662e122
Alvanza Saputra <alvanzasaputra123734@gmail.com>
2025-06-06 20:11:37 +0700

    fix: dashboard admin & identitas user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,30 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Blade::directive('getStatusColor', function ($status) {
+            return "<?php 
+                switch ($status) {
+                    case 'selesai': echo 'success'; break;
+                    case 'proses': echo 'warning'; break;
+                    case 'menunggu': echo 'danger'; break;
+                    case 'ditolak': echo 'info'; break;
+                    default: echo 'secondary';
+                }
+            ?>";
+        });
+
+        Blade::directive('getStatusIcon', function ($status) {
+            return "<?php 
+                switch ($status) {
+                    case 'selesai': echo 'check-circle'; break;
+                    case 'proses': echo 'time'; break;
+                    case 'menunggu': echo 'error'; break;
+                    case 'ditolak': echo 'x-circle'; break;
+                    default: echo 'help-circle';
+                }
+            ?>";
+        });
     }
 }
