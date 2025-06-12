9732f6c099c8d40876b7ced43ec08897d1f320a7
Ilham FT <ilhamft.magang.om@jagoanhosting.com>
2025-06-12 10:41:01 +0700

    fix-server-bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         Blade::directive('getStatusColor', function ($status) {
             return "<?php 
                 switch ($status) {
